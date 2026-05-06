aaa67970dd286191182b15710ac79be0ec068236
Tamarazbl <tamara.zobel@icloud.com>
2026-05-06 07:52:25 +0200

    Klassen, Servies, Bericht-Page erstellt

---
+++ Models/Wochenbericht.cs
@@ -0,0 +1,17 @@
+﻿namespace xAzubiLog.Models
+{
+    public class Wochenbericht
+    {
+        public int Id { get; set; }
+        public int BenutzerId { get; set; }
+        public User Benutzer { get; set; } = null!;
+        public int Kalenderwoche { get; set; }
+        public double Gesamtstunden { get; set; }
+        public int Jahr { get; set; }
+        public string Status { get; set; } = "";
+        public string Kommentar { get; set; } = "";
+        public DateTime ErstelltAm { get; set; } = DateTime.Now;
+
+        public List<BerichtEintrag> BerichtEintraege { get; set; } = new();
+    }
+}
