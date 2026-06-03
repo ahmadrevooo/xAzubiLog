8934847a56aab55648f2ac26b4e5d33ea400efd5
Tamarazbl <tamara.zobel@icloud.com>
2026-06-03 13:49:33 +0200

    Migration: PasswortResetTokens + BerichtEintraege Fix

--- Data/AzubiLog_BlazorContext.cs
+++ Data/AzubiLog_BlazorContext.cs
@@ -11,7 +11,7 @@ namespace xAzubiLog.Data
         }
         public DbSet<User> User { get; set; } = default!;
         public DbSet<Wochenbericht> Wochenbericht { get; set; } = default!;
-        public DbSet<BerichtEintraege> BerichtEintrag { get; set; } = default!;
+        public DbSet<BerichtEintraege> BerichtEintraege { get; set; } = default!;
 
         // Compatibility wrappers for older code that referenced pluralized DbSet names
         public IQueryable<User> Users => User;
